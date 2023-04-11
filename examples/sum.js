a6012c170af06884f77303544ea1088dcb095a8a
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2023-04-11 21:20:03 +0100

    add progress bar to example

--- examples/sum.js
+++ examples/sum.js
@@ -12,7 +12,31 @@ const sumElm = new Elm(
   "strong",
   Reactor.computed(() => `Sum is ${lhs.value + rhs.value || 0}`),
 );
+const currentProgress = new Reactor(0);
+
+const func = () => {
+  currentProgress.value += 1;
+  if (currentProgress.value < 100) {
+    timer = setTimeout(func, Math.floor(Math.random() * 100));
+  } else {
+    clearTimeout(timer);
+  }
+};
+let timer = setTimeout(func, 20);
+const progressElm = new Elm("progress").bind({
+  style: "display: block",
+  min: 0,
+  max: 100,
+  value: Reactor.computed(() => currentProgress.value),
+});
 
 document
   .getElementById("app")
-  .append(lhsElm.$el, plusElm.$el, rhsElm.$el, equalElm.$el, sumElm.$el);
+  .append(
+    lhsElm.$el,
+    plusElm.$el,
+    rhsElm.$el,
+    equalElm.$el,
+    sumElm.$el,
+    progressElm.$el,
+  );
